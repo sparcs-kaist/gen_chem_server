284e79e415339bee2826da5e9ebebf94562e66e4
jaejun1 <ltq3021@gmail.com>
2017-11-26 00:10:02 +0900

    implement model

--- ch101/models.py
+++ ch101/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class notice(models.Model):
+    title = models.TextField()
+    description = models.TextField()
+    event_date = models.DateTimeField()
+    post_date = models.DateTimeField()
+
+
+class schedule(models.Model):
+    title = models.TextField()
+    description = models.TextField()
+    event_date = models.DateTimeField()
+    type = models.TextField()
+    class_name = models.TextField()
+
+
+class download(models.Model):
+    name = models.TextField()
+    date = models.DateTimeField()
+    link = models.TextField()
+    type = models.TextField()
+
+
+class contact(models.Model):
+    name = models.TextField()
+    phone = models.TextField()
+    email = models.TextField()
+    picture = models.TextField()
--- ch102/models.py
+++ ch102/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+
+
+class notice(models.Model):
+    title = models.TextField()
+    description = models.TextField()
+    event_date = models.DateTimeField()
+    post_date = models.DateTimeField()
+
+
+class schedule(models.Model):
+    title = models.TextField()
+    description = models.TextField()
+    event_date = models.DateTimeField()
+    type = models.TextField()
+    class_name = models.TextField()
+
+
+class evaluation(models.Model):
+    description = models.TextField()
+
+
+class safety(models.Model):
+    description = models.TextField()
+
+
+class links(models.Model):
+    title = models.TextField()
+    description = models.TextField()
+    link = models.TextField()
+
+
+class contact(models.Model):
+    name = models.TextField()
+    phone = models.TextField()
+    email = models.TextField()
+    picture = models.TextField()
