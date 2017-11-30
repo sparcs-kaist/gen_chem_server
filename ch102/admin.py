ba24d081047b1d8257399787bc4e701c70f32b96
Sunmin <allnameswhy@gmail.com>
2017-11-30 18:45:13 +0900

    Enable model registration in admin in ch102

--- ch102/admin.py
+++ ch102/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.db import models
+from ch102.models import *
 
 # Register your models here.
+
+admin.site.register(Notice)
+admin.site.regiter(Schedule)
+admin.site.register(Evaluation)
+admin.site.register(Safety)
+admin.site.register(Links)
+admin.site.register(Contact)
